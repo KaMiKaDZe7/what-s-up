ed8371a40a4dc170b38801bf2ad7a39a669a4756
Alexey Efimov <alexgta05@gmail.com>
2019-01-28 16:44:43 +0400

    28.01.2019

---
+++ 1.js
@@ -0,0 +1,40 @@
+/*
+boolean_var_alert_console.log_if_else if_string_prompt_object_number
+
+
+
+*/
+//alert a,b
+/*
+var a, b;
+a=5;
+b=10;
+var c=a/b;
+alert(c);
+*/
+//типы данных boolean, number, string, null, undefined, object
+//задачи
+//задача 2: найти знач. выражения
+/*
+var a, b, c1, c2, c3, c4;
+a=5; b=3;
+c1=a+b; alert (c1 + ' - Ответ c1');
+c2=a-b; alert (c2 + ' - Ответ c2');
+c3=a*b; alert (c3 + ' - Ответ c3');
+c4=a/b; alert (c4 + ' - Ответ C4');
+//
+*/
+
+/*
+//if else
+var year = prompt('what yers?');
+if (year<2019) {alert('рано, не этот год');}
+else if (year>2019) {alert ('поздно, этот год ещё не настал');}
+else if (year=2019) {alert('да, этот год');}
+console.log(year);
+*/
+//
+
+//
+
+//
---
+++ quest.js
@@ -0,0 +1,30 @@
+//ctrl+j
+//задачи
+//Задача 1: определить типы даных
+//дано: str, num, flag, txt.
+
+/*
+var str="Привет";
+var num=123;
+var flag=true;
+var txt={name="igor"};
+alert ("Привет");
+*/
+//задача 2: Найти знач. выражения
+//дано: a=5, b=3 найти c1,c2,c3,c4.
+var a, b, c1, c2, c3, c4;
+a=5; b=3;
+c1=a+b; alert 
+c2=a-b; alert 
+c3=a*b; alert 
+c4=a/b; alert 
+(c1 +' - Ответ c1 \n'+
+c2 + ' - Ответ c2 \n'+
+c3 + ' - Ответ c3 \n'+
+c4 + ' - Ответ C4');
+/*
+c1=a+b; alert (c1 + ' - Ответ c1');
+c2=a-b; alert (c2 + ' - Ответ c2');
+c3=a*b; alert (c3 + ' - Ответ c3');
+c4=a/b; alert (c4 + ' - Ответ C4');
+*/
